483e16681f8a7c026f4a99ec29ea888ccfcb2219
hack2root <me@experimentalcommunity.org>
2019-02-25 11:04:22 +0300

    initial commit

---
+++ git-create.ps1
@@ -0,0 +1,7 @@
+& echo "# read-matrix" > README.md
+& echo "https://stackoverflow.com/questions/54855202/c-sharp-read-text-file-and-create-multidimensional-array-from-it/54857698#54857698" >> README.md
+& git init
+& git add .
+& git commit -m "initial commit"
+& git remote add origin https://github.com/hack2root/read-matrix.git
+& git push -u origin master
